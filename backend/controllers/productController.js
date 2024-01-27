7738b7df1c640d15e1a2f2c16d1b267bc6ac0e35
Ajay v <vajay4834@gmail.com>
2024-01-27 17:38:09 +0530

    router and controllers created

---
+++ backend/controllers/productController.js
@@ -0,0 +1,12 @@
+exports.getProducts=(req,res,next)=>{
+    res.json({
+        success:true,
+        message:'Get Products working'
+    })
+}
+exports.getSingleProducts=(req,res,next)=>{
+    res.json({
+        success:true,
+        message:'Get single Products working'
+    })
+}
