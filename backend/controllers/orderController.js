7321bdbfff1e250f4779f9fe2950e34c78d07956
Ajay v <vajay4834@gmail.com>
2024-01-28 11:25:31 +0530

    backend completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,12 +1,14 @@
 const orderModel=require('../models/ordermodel')
-exports.createOrder=(req,res,next)=>{
+exports.createOrder=async(req,res,next)=>{
    const cartItems=req.body;
 
-   const amount=cartItems.reduce((acc,item)=>(acc+item.product.price*item.qty),0);
-   console.log(amount)
+   const amount= Number(cartItems.reduce((acc,item)=>(acc+item.product.price*item.qty),0)).toFixed(2);
+  const status= 'pending';
+ const order= await orderModel.create({cartItems,amount,status})
+
     res.json({
         success:true,
-        message:'order  working'
+        order
 
     })
 }
