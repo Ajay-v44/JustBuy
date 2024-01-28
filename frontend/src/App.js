0c0e27faa2a80226f130c557900d949f97f2cc84
Ajay v <vajay4834@gmail.com>
2024-01-28 15:19:06 +0530

    detail product added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,15 +2,20 @@ import "./App.css";
 import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Pages/Home";
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ProductDetail from "./Pages/ProductDetail";
+
 function App() {
   return (
+    
     <div className="App">
       <Router>
         <Header/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/search" element={<Home />} />
+          <Route path='/product/:id' element={<ProductDetail/>}/>
         </Routes>
         <Footer/>
       </Router>
