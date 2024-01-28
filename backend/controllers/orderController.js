181579d651e72082963b841f8ae163af29ed0640
Ajay v <vajay4834@gmail.com>
2024-01-28 19:06:02 +0530

    backend completed

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,6 @@
 const orderModel = require("../models/ordermodel");
+const productModel = require("../models/productModel");
+
 exports.createOrder = async (req, res, next) => {
   const cartItems = req.body;
 
@@ -7,7 +9,12 @@ exports.createOrder = async (req, res, next) => {
   ).toFixed(2);
   const status = "pending";
   const order = await orderModel.create({ cartItems, amount, status });
-
+  //updating stock
+  cartItems.forEach(async (item) => {
+    const products =await productModel.findById(item.product._id);
+    products.stock = products.stock - item.qty;
+    await products.save();
+  });
   res.json({
     success: true,
     order,
